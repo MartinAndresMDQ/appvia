5169dc6ec1b0155455fb2d6099dd63aad380b09f
MartinAndresMDQ <martinandres987@gmail.com>
2024-07-01 12:28:31 -0300

    full commit

--- src/components/icons/save.tsx
+++
@@ -1,5 +0,0 @@
-import {faFloppyDisk} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
-export const title = 'Save + Load';
-export const component = () => <FontAwesomeIcon icon={faFloppyDisk} />;
